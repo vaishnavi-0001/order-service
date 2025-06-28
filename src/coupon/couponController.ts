c657c0653ec245955fe81865adf3b20e86b43b42
vaishnavi-0001 <140255751+vaishnavi-0001@users.noreply.github.com>
2025-06-28 16:13:42 +0530

    Add validate coupon endpoint

--- src/coupon/couponController.ts
+++ src/coupon/couponController.ts
@@ -1,5 +1,6 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import couponModel from "./couponModel";
+import createHttpError from "http-errors";
 
 export class CouponController {
   create = async (req: Request, res: Response) => {
@@ -21,4 +22,28 @@ export class CouponController {
   };
 
   // todo: Complete CRUD assignment. This will be used in dashboard.
+
+  verify = async (req: Request, res: Response, next: NextFunction) => {
+    const { code, tenantId } = req.body;
+
+    // todo: request validation
+
+    // todo: add service layer with dependency injection.
+    const coupon = await couponModel.findOne({ code, tenantId });
+
+    if (!coupon) {
+      const error = createHttpError(400, "Coupon does not exists");
+      return next(error);
+    }
+
+    // validate expiry
+    const currentDate = new Date();
+    const couponDate = new Date(coupon.validUpto);
+
+    if (currentDate <= couponDate) {
+      return res.json({ valid: true, discount: coupon.discount });
+    }
+
+    return res.json({ valid: false, discount: 0 });
+  };
 }
--- src/coupon/couponRouter.ts
+++ src/coupon/couponRouter.ts
@@ -6,5 +6,6 @@ import { CouponController } from "./couponController";
 const router = express.Router();
 const couponController = new CouponController();
 router.post("/", authenticate, asyncWrapper(couponController.create));
+router.post("/verify", authenticate, asyncWrapper(couponController.verify));
 
 export default router;
